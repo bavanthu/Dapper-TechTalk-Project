8c70ac7c5464872a2dfc41ae45ddbc57992b8791
thamee24@outlook.com <thamee24@outlook.com>
2020-08-25 11:06:43 +0530

    Add project files.

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using Dapper;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        [HttpGet]
+
+        public ActionResult Index()
+        {
+            return View(DapperORM.ReturnList<EmployeeModel>("EmployeeViewAll"));
+        }
+
+
+       [HttpGet]
+       public ActionResult AddOrEdit(int id=0)
+       {
+          return View();
+
+        
+       }
+
+
+        [HttpPost]
+        public ActionResult AddOrEdit(EmployeeModel emp)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@EmployeeID", emp.EmployeeID);
+            param.Add("@Name", emp.Name);
+            param.Add("@Position", emp.Position);
+            param.Add("@Age", emp.Age);
+            param.Add("@Salary", emp.Salary);
+            DapperORM.ExecuteWithoutReturn("EmployeeAddOrEdit", param);
+            return RedirectToAction("Index");
+        
+        }
+    }
+}
---
+++ WebApplication1/Models/DapperORM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+
+namespace WebApplication1.Models
+{
+    public static class DapperORM
+    {
+        private static string connectionstring = @"Server=54.36.224.103\MSSQL2017;Database=DapperDB;User Id=UniSQL;Password=Lcgytr@cGGH103;";
+
+
+        public static void ExecuteWithoutReturn(string EmployeeAddOrEdit, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionstring))
+            {
+
+                sqlcon.Open();
+                sqlcon.Execute(EmployeeAddOrEdit, param, commandType: CommandType.StoredProcedure);
+            }
+
+
+        }
+
+        //DapperORM.ExecuteReturnScalar<int>(_,_);
+        public static T ExecuteReturnScalar<T>(string EmployeeAddOrEdit, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionstring))
+            {
+
+                sqlcon.Open();
+                return (T)Convert.ChangeType(sqlcon.ExecuteScalar(EmployeeAddOrEdit, param, commandType: CommandType.StoredProcedure),typeof(T));
+            }
+        }
+
+
+        //DapperORM.ReturnList<EmployeeModel> <= IEnumerable<EmployeeModel>
+        public static IEnumerable<T> ReturnList<T>(string EmployeeAddOrEdit, DynamicParameters param= null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionstring))
+            {
+
+                sqlcon.Open();
+                return sqlcon.Query<T>(EmployeeAddOrEdit, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+
+    }
+}
